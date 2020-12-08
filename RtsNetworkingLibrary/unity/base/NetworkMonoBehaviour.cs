67a7d92bf9c3d6b83d05b01400bd65e0f8b1c91a
Florian Lutze <lutze.florian@gmail.com>
2020-12-08 10:04:22 +0100

    Removed all ignored files

--- RtsNetworkingLibrary/unity/base/NetworkMonoBehaviour.cs
+++ RtsNetworkingLibrary/unity/base/NetworkMonoBehaviour.cs
@@ -12,6 +12,8 @@ namespace RtsNetworkingLibrary.unity.@base
         private NetworkManager _networkManager;
         public int clientId;
         public ulong entityId;
+
+        public bool IsLocalPlayer { get; private set; } = true;
         
         // Todo get a list of syncvars and check if they have changed
         
@@ -21,8 +23,11 @@ namespace RtsNetworkingLibrary.unity.@base
             if (_networkManager == null)
             {
                 throw new Exception(
-                    "Network Manager not found! You should have a GameObject called \"NetworkManager\" in your project, that has the NetworkManager script on it!");
+                    "Network Manager not found! You should have a GameObject called \"NetworkManager\" in your project, that has the <NetworkManager> script on it!");
             }
+
+            IsLocalPlayer = (_networkManager.ClientId == clientId);
+            Debug.Log("Is local player: " + IsLocalPlayer);
             FieldInfo[] objectFields = this.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
             for (int i = 0; i < objectFields.Length; i++)
             {
@@ -34,6 +39,8 @@ namespace RtsNetworkingLibrary.unity.@base
 
         private void Update()
         {
+            if (_networkManager.ClientId != clientId)
+                return;
             if(_networkManager.IsServer)
                 ServerUpdate();
             ClientUpdate();
